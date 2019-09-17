73a419c7c22214237476b8edbe5eef9283ff0eaa
unknown <seglifabian@gmail.com>
2019-09-17 18:24:16 -0500

    update

--- 01week/datatypes.js
+++ 01week/datatypes.js
@@ -1,4 +1,74 @@
+//Day and time
+var now = new Date();
+console.log ("The time at the beep is",now);
+
+//number to a string
+ var x = 15;
+ var y = x.toString();
+ console.log(y);
+  
+//String to a number
 var x = 5;
 var y = "10";
 var yInt = Number.parsInt(y);
 console.log(x +yInt);
+
+
+//Boolean
+var blue = "true";
+console.log(typeof(blue));
+
+//null
+var red = null;
+console.log(red);
+
+//undefined
+var yellow 
+console.log(yellow);
+
+//number
+var green = 9;
+console.log(typeof(green));
+
+//NaN
+var white = '8';
+if (white !== 8) {
+console.log('NaN');
+}
+
+//string
+var beige = "10";
+console.log(typeof(beige));
+
+//add two numbers together 
+var orange = 32;
+var pink = 23;
+console.log(orange / pink);
+
+//two things are true
+var cat = 8;
+var dog = 7;
+if (cat > 0  && dog > 0) {
+  console.log("both are true");
+}
+
+//false
+var cat = 5;
+var dog = 4;
+if ( cat > 0 || dog > 5) {
+console.log("one is true")
+} else {
+ console.log("both are false");
+}
+
+//both things are not true
+var cat = 7;
+var dog = 9;
+if ( cat == false && dog == false) {
+ console.log("bingo!!");
+  
+}
+
+
+
+
