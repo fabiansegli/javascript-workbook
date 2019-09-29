0496bd0deda3a94023b615fc40341920210163fe
unknown <seglifabian@gmail.com>
2019-09-29 13:16:51 -0500

    Work in progress

---
+++ 03week/warmup.js
@@ -0,0 +1,29 @@
+//Fizz Buzz
+
+//  for (i=1; i<=100; i++) {
+//    if(i%3 === 0 && i%5 ===0) { // || if (i%15 === 0) {
+
+//     console.log (i, "FizzBuzz");
+// }       
+
+//     else if (i%3 === 0) {
+//     console.log (i, "Fizz"); 
+// }
+//     else if (i%5 === 0) {
+//     console.log(i, "Buzz");
+// }      
+   
+   
+//     else {
+//     console.log(i);
+// }    
+// }
+
+// i++ same as ++i 
+
+// var i = 1;
+// var x = 1;
+// i++
+// console.log("i =", i);
+// console.log("updated i =", ++i);
+
